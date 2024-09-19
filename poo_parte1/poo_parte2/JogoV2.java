2e609276dbe712f2144a84500e41410a9a412ae7
Pedro Lucas <pedrolucasrferraz@gmail.com>
2024-09-19 17:48:06 -0300

    feat: implementa a versao 2 do jogo

--- poo_parte1/poo_parte2/JogoV2.java
+++ poo_parte1/poo_parte2/JogoV2.java
@@ -1,9 +1,55 @@
+import java.util.Random;
 public class JogoV2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Começando. .");
         var cacador = new Personagem();
+        var soneca = new Personagem(2, 4, 10);
+        var fominha = new Personagem(6, 10, 2);
         cacador.nome = "Herobrine";
+        soneca.nome = "Ursinho Pooh";
+        fominha.nome = "Scooby";
+        var gerador = new Random();
         while (true){
-            cacador.cacar();
+            var acaoDoCacador = gerador.nextInt(3) + 1;
+            switch(acaoDoCacador){
+                case 1:
+                    cacador.cacar();
+                    break;
+                case 2:
+                    cacador.comer();
+                    break;
+                case 3:
+                    cacador.dormir();
+                    break;
+            }
+            System.out.println(cacador);
+            System.out.println("----------------------------------");
+            var acaoDoSoneca = gerador.nextInt(3) + 1;
+            if(acaoDoSoneca == 1){
+                soneca.cacar();
+            }
+            else if (acaoDoSoneca == 2) {
+                soneca.comer();
+            }
+            else{
+                soneca.dormir();
+            }
+            System.out.println(soneca);
+            System.out.println("----------------------------------");
+            switch(gerador.nextInt(3) + 1){
+                case 1:
+                    fominha.cacar();
+                    break;
+                case 2:
+                    fominha.comer();
+                    break;
+                case 3:
+                    fominha.dormir();
+                    break;
+            }
+            System.out.println(fominha);
+            System.out.println("==================================");
+            Thread.sleep(10000);
         }
     }
 }
--- poo_parte1/poo_parte2/Personagem.java
+++ poo_parte1/poo_parte2/Personagem.java
@@ -1,19 +1,31 @@
 public class Personagem{
     String nome;
-    int energia = 10;
-    int fome = 0;
-    int sono = 0;
+    private int energia;
+    private int fome;
+    private int sono;
 
+    Personagem(){
+        System.out.println("Construindo novo personagem");
+        energia = 10;
+        fome = 0;
+        sono = 0;
+    }
+
+    Personagem(int energia, int fome, int sono){
+        System.out.println("Construindo novo personagem");
+        this.energia = energia < 0 || energia > 10 ? 10 : energia;
+        this.fome = fome = fome >= 0 && fome <= 10 ? fome : 0;
+        this.sono = sono = sono >= 0 && sono <= 10 ? sono : 0;
+    }
     void cacar(){
         if(energia >= 2){
             System.out.printf("%s caçando\n", nome);
             energia -= 2;
         }else{
             System.out.printf("%s sem energia para caçar", nome);
-            fome = fome == 10 ? fome : fome + 1;
-            sono = sono == 10 ? sono : sono + 1;
         }
-        
+        fome = fome == 10 ? fome : fome + 1;
+        sono = sono == 10 ? sono : sono + 1;
     }
 
     void comer(){
@@ -35,5 +47,10 @@ public class Personagem{
         else{
             System.out.println(nome + " sem sono");
         }
+    }
+
+    public String toString(){
+        return String.format("%s: e:%d, f:%d, s:%d",
+        nome, energia, fome, sono);
     }
 }
